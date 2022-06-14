1a1905934697f3ea34716253ff9b85bc56e3e2a7
mlatus <wqseleven@gmail.com>
2022-06-14 17:45:36 +0800

    init
    Co-authored-by: Lin Yinfeng <lin.yinfeng@outlook.com>

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Database for minecraft launcher.";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    with flake-utils.lib;
+    with nixpkgs.lib;
+    with builtins;
+    let
+      importJSONFiles = dir:
+        listToAttrs (map (fp: {
+          name = removeSuffix ".json" (baseNameOf fp);
+          value = importJSON fp;
+        }) (filesystem.listFilesRecursive dir));
+    in eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        py = pkgs.python3.withPackages (p: [ p.requests ]);
+      in {
+        apps.update = mkApp {
+          drv = let
+            snippet = dir: ''
+              pushd ./${dir}
+              ${py}/bin/python update.py
+              popd
+            '';
+          in pkgs.writeShellScriptBin "update" ''
+            ${snippet "fabric"}
+            ${snippet "vanilla"}
+          '';
+        };
+      }) // {
+        manifests = importJSON ./vanilla/manifests.json;
+        versions = importJSONFiles ./vanilla/versions;
+        assets = importJSONFiles ./vanilla/asset_indices;
+        fabric.profiles = importJSON ./fabric/profiles.json;
+        fabric.libraries = importJSON ./fabric/libraries.json;
+        fabric.loaders = importJSON ./fabric/loaders.json;
+      };
+}
